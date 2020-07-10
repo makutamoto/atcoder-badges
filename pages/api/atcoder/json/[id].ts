9000d1e5f13b4e0d770d2b98924119af49252185
Makutamoto <34860673+makutamoto@users.noreply.github.com>
2020-07-11 01:08:42 +0900

    + JSON endpoint.

---
+++ pages/api/atcoder/json/[id].ts
@@ -0,0 +1,29 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+
+import { getUserRateWithCache } from '../../../../lib/atcoder';
+
+const colors = [
+    "808080", // gray
+    "804000", // brown
+    "008000", // green
+    "00C0C0", // cyan
+    "0000FF", // blue
+    "C0C000", // yellow
+    "FF8000", // orange
+    "FF0000", // red
+];
+
+const json = (rate: number | null) => `\
+{
+    "schemaVersion": 1,
+    "label": "AtCoder",
+    "message": "${rate === null ? '-' : rate}",
+    "color": "#${rate === null ? '000000' : colors[Math.floor(Math.min(2800, rate) / 400)]}"
+}`;
+
+export default async function (req: NextApiRequest, res: NextApiResponse) {
+    let username = req.query.id as string;
+    let rate = await getUserRateWithCache(username);
+    res.setHeader('Content-type', 'application/json');
+    res.status(200).send(json(rate));
+}
