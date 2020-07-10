7f39ec3582532e1e46cb1f107ea55b862557ef4d
Makutamoto <34860673+makutamoto@users.noreply.github.com>
2020-07-11 01:44:51 +0900

    + changed url.

--- components/Generator.tsx
+++ components/Generator.tsx
@@ -2,7 +2,8 @@ import { useState, useEffect } from 'react';
 import CopyField from './CopyField';
 
 const userURL = (name: string) => `https://atcoder.jp/users/${name}`;
-const imageLink = (name: string) => process.browser ? `${window.location.origin}/api/atcoder/${name}` : '';
+const dataLink = (name: string) => process.browser ? `${window.location.origin}/api/atcoder/json/${name}` : '';
+const shieldsioLink = (url: string) => `https://img.shields.io/endpoint?url=${encodeURIComponent(url)}`;
 
 export interface GeneratorProps {
     name: string,
@@ -12,7 +13,7 @@ export default function(props: GeneratorProps) {
     let [image, setImage] = useState('');
     useEffect(() => {
         setUser(userURL(props.name));
-        setImage(imageLink(props.name));
+        setImage(shieldsioLink(dataLink(props.name)));
     }, [props.name]);
     return (
         <>
