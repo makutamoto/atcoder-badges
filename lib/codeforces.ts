d92c11ab746e9ecb1e45851f11bf61930c991ee0
Makutamoto <34860673+makutamoto@users.noreply.github.com>
2020-07-11 17:03:54 +0900

    + Fixed codeforces.

--- lib/codeforces.ts
+++ lib/codeforces.ts
@@ -5,15 +5,19 @@ export interface ContestResult {
 }
 
 export interface RatingHistory {
-    result?: ContestResult[],
+    result: ContestResult[],
 }
 
 const ratingHistroyURL = (name: string) => `https://codeforces.com/api/user.rating?handle=${name}`;
 
 export async function fetchCodeforcesRate(name: string): Promise<number | null> {
-    let data: RatingHistory;
-    console.log(`Fetching '${name}'...`);
-    data = (await axios.get(ratingHistroyURL(name))).data;
-    if(data.result === undefined || data.result.length === 0) return null;
-    return data.result[data.result.length - 1].newRating;
+    try {
+        let data: RatingHistory;
+        console.log(`Fetching '${name}'...`);
+        data = (await axios.get(ratingHistroyURL(name))).data;
+        if(data.result.length === 0) return null;
+        return data.result[data.result.length - 1].newRating;
+    } catch {
+        return null;
+    }
 }
